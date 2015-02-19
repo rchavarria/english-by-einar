d633705d15b02d902dee41a6aa1378db293f4dfd
rchavarria <txingo@gmail.com>
2015-02-19 01:25:22 +0100

    Use JavaScript code from the first ionic app

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,83 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('playSound', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
+
+.controller('ExampleController', function($scope, $cordovaMedia, Vocabulary) {
+
+    var media;
+
+    $scope.word = Vocabulary.next();
+
+    $scope.play = function (src) {
+        if (media) {
+            $scope.stop();
+        }
+
+        media = $cordovaMedia.newMedia(src);
+        media.play();
+    };
+
+    $scope.stop = function () {
+        if (!media) {
+            return;
+        }
+
+        media.stop();
+        media.release();
+    };
+
+    $scope.next = function () {
+        $scope.word = Vocabulary.next();
+    };
+ 
+})
+
+.factory('Vocabulary', function() {
+    var current = 0,
+        words = [];
+
+    words.push({
+        thumbnail: 'img/small-dog.jpg',
+        thumbnailTitle: 'Dog#1',
+        thumbnailDescription: 'Wouf! Wouf!',
+        image: 'img/dog.jpg',
+        imageDescription: 'Help your kid to say that in English',
+        audio: '/android_asset/www/audio/some-demo.mp3'
+    });
+    words.push({
+        thumbnail: 'img/small-dog.jpg',
+        thumbnailTitle: 'Dog#2',
+        thumbnailDescription: 'Wouf! Wouf!',
+        image: 'img/dog.jpg',
+        imageDescription: 'Help your kid to say that in English',
+        audio: '/android_asset/www/audio/some-demo.mp3'
+    });
+    words.push({
+        thumbnail: 'img/small-dog.jpg',
+        thumbnailTitle: 'Dog#3',
+        thumbnailDescription: 'Wouf! Wouf!',
+        image: 'img/dog.jpg',
+        imageDescription: 'Help your kid to say that in English',
+        audio: '/android_asset/www/audio/some-demo.mp3'
+    });
+
+    return {
+        next: function () {
+            var word = words[current];
+            current = (current + 1) % words.length;
+            return word;
+        }
+    };
+});
+
