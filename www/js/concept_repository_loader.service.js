d20dd44448fb2ac4b8c84a43eee81af596886b77
rchavarria <txingo@gmail.com>
2015-02-28 13:23:08 +0100

    Fix first Concept values

--- www/js/concept_repository_loader.service.js
+++ www/js/concept_repository_loader.service.js
@@ -5,9 +5,9 @@
     app.service('ConceptRepositoryLoader', ['ConceptBuilder', 'ConceptRepository', function (ConceptBuilder, ConceptRepository) {
         this.load = function () {
             var concept = ConceptBuilder.init()
-                .withThumbnail('dog', 'dog', 'dog')
-                .withImage('dog', 'dog')
-                .withAudio('dog')
+                .withThumbnail('img/small-dog.jpg', 'Dog#1', 'Wouf! Wouf!')
+                .withImage('img/dog.jpg', 'Help your kid to say that in English')
+                .withAudio('/android_asset/www/audio/some-demo.mp3')
                 .build();
 
             ConceptRepository.addConcept(concept);
