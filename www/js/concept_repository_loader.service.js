d07aefc5a82193db9098a4d34ad9a07258940fbd
rchavarria <txingo@gmail.com>
2015-02-28 13:31:51 +0100

    Inline var

--- www/js/concept_repository_loader.service.js
+++ www/js/concept_repository_loader.service.js
@@ -4,13 +4,12 @@
 
     app.service('ConceptRepositoryLoader', ['ConceptBuilder', 'ConceptRepository', function (ConceptBuilder, ConceptRepository) {
         this.load = function () {
-            var concept = ConceptBuilder.init()
+            ConceptRepository.addConcept(ConceptBuilder
+                .init()
                 .withThumbnail('img/small-dog.jpg', 'Dog#1', 'Wouf! Wouf!')
                 .withImage('img/dog.jpg', 'Help your kid to say that in English')
                 .withAudio('/android_asset/www/audio/some-demo.mp3')
-                .build();
-
-            ConceptRepository.addConcept(concept);
+                .build());
         };
     }]);
 
